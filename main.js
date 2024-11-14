e97e5da0b58a448a77b5ee3d6c2a40a372a03cdd
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-11-14 10:11:39 +0700

    hide pokemon sprite

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const material = new THREE.SpriteMaterial({
 
 // For Ghost
 // Modify the value to change the sprite in screen
-var spriteIndex = 1;
+var spriteIndex = 0;
 
 material.map.offset = new THREE.Vector2(0.2 * spriteIndex, 0);
 material.map.repeat = new THREE.Vector2(1 / 5, 1);
@@ -29,27 +29,27 @@ material.map.repeat = new THREE.Vector2(1 / 5, 1);
 // Create sprite and add to scene
 const sprite = new THREE.Sprite(material);
 sprite.scale.set(250, 250, 250)
-sprite.position.set(-500, 0, 0);
+sprite.position.set(0, 0, 0);
 scene.add(sprite);
 
 // For Pokemon
-const pokemonMap = new THREE.TextureLoader().load('./texture/diamond-pearl.png');
-const pokemonMaterial = new THREE.SpriteMaterial({ 
-  map: pokemonMap, 
-  sizeAttenuation: false,
-});
+// const pokemonMap = new THREE.TextureLoader().load('./texture/diamond-pearl.png');
+// const pokemonMaterial = new THREE.SpriteMaterial({ 
+//   map: pokemonMap, 
+//   sizeAttenuation: false,
+// });
 
-var spriteIndexX = 0; // start from left, indexes is 0-27
-var spriteIndexY = 8; // start from bottom, indexes is 0-17
+// var spriteIndexX = 0; // start from left, indexes is 0-27
+// var spriteIndexY = 8; // start from bottom, indexes is 0-17
 
-pokemonMaterial.map.offset = new THREE.Vector2((1/28) * spriteIndexX, 1/18 * spriteIndexY);
-pokemonMaterial.map.repeat = new THREE.Vector2(1 / 28, 1 / 18);
+// pokemonMaterial.map.offset = new THREE.Vector2((1/28) * spriteIndexX, (1/18) * spriteIndexY);
+// pokemonMaterial.map.repeat = new THREE.Vector2(1 / 28, 1 / 18);
 
-// Create sprite and add to scene
-const pokemonSprite = new THREE.Sprite(pokemonMaterial);
-pokemonSprite.scale.set(500, 500, 500)
-pokemonSprite.position.set(500, 0, 0);
-scene.add(pokemonSprite);
+// // Create sprite and add to scene
+// const pokemonSprite = new THREE.Sprite(pokemonMaterial);
+// pokemonSprite.scale.set(500, 500, 500);
+// pokemonSprite.position.set(500, 0, 0);
+// scene.add(pokemonSprite);
 
 // Animation loop
 function animate() {
--- perspective.js
+++ perspective.js
@@ -5,7 +5,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.OrthographicCamera(-window.innerWidth, window.innerWidth, window.innerHeight, -window.innerHeight, 1, 500);
+// const camera = new THREE.OrthographicCamera(-window.innerWidth, window.innerWidth, window.innerHeight, -window.innerHeight, 1, 500);
 const perspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 perspectiveCamera.position.set(0, 0, 5);
 perspectiveCamera.lookAt(0, 0, 0);
